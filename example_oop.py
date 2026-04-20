90527f18c508b6d7732ebf104b60d2fd5d355c76
raynalda <raynalda@udel.edu>
2026-04-20 11:13:53 -0400

    Add example_oop.py

--- example_oop.py
+++ example_oop.py
@@ -1,6 +1,6 @@
 
 class Counter: 
-    def __init__(self, start: int=0) -> None: 
+    def __init__(self, start: int=0, step: int=1) -> None: 
         self.value = start
 
     def increment(self) -> None: 
@@ -8,7 +8,9 @@ class Counter:
 
     def current_value(self) -> int: 
         return self.value
-
+    
+    def increment_2(self):
+        self.value += 2
 
 c=Counter(start=5) #creating an object from the Counter class
 
@@ -19,3 +21,6 @@ c1.increment() #calling the increment method on the object c1
 
 print(f"c1: {c1.current_value()}") #calling the current_value method on the object c1 and printing the result
 print(f"c1: {c1.current_value()}")
+
+c.increment_2() #calling the increment_2 method on the object c
+print(f"c: {c.current_value()}") #calling the current_value method on the object c and printing the result
