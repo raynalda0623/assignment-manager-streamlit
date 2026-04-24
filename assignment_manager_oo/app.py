0ec28e987c23bb53902e6af760573c8037862719
raynalda <raynalda@udel.edu>
2026-04-24 11:15:39 -0400

    refactorization

--- assignment_manager_oo/app.py
+++ assignment_manager_oo/app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from assignment_manager_oo.services.assignment_manager import AssignmentManager 
+from assignment_manager_oo.data.assignment_store import AssignmentStore
 
 st.set_page_config("Assignment Manager")
 
@@ -15,7 +17,12 @@ if "page" not in st.session_state:
 
 if st.session_state["logged_in"]:
     if st.session_state["role"] == "Instructor":
-        pass
+        #create an object from the data class and load the data
+        from pathlib import Path
+        store = AssignmentStore(Path("assignments.json"))
+        manager = AssignmentManager(store.load())
+
+        
     elif st.session_state["role"] == "Student":
         pass
 else:
--- assignment_manager_oo/ui/assignment_dashboard.py
+++ assignment_manager_oo/ui/assignment_dashboard.py
@@ -1,14 +1,27 @@
 import streamlit as st
+from assignment_manager_oo.services.assignment_manager import AssignmentManager 
+from assignment_manager_oo.data.assignment_store import AssignmentStore
 
 class AssignmentDashboard:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, manager: "AssignmentManager", store: "AssignmentStore") -> None:
+        self.manager = manager
+        self.store = store
+
 
     def main(self):
-        pass
+        if st.session_state["page"] == "dashboard":
+            self.show_manage_assignments()
+        else:
+            self.show_add_new_assignment()
 
     def show_manage_assignments(self):
-        pass
+        assignments = self.manager.all()
+        for assignment in assignments:
+            st.write(f"Title: {assignment['title']}")
+
+            if st.button("Add New Assignment"):
+                st.session_state["page"] = "add_assignment"
+                st.rerun()
 
     def show_add_new_assignment(self):
         pass
